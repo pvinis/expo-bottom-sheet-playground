61282bfdfa1c4d79e220fa6a34d082a8e0732f99
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-10-16 19:08:03 +0100

    fix: use js stack with animations etc

--- src/app/bottom/_layout.tsx
+++ src/app/bottom/_layout.tsx
@@ -1,3 +1,19 @@
-import { Slot } from "expo-router"
+// import { Slot } from "expo-router"
+import { ParamListBase, StackNavigationState } from "@react-navigation/native"
+import {
+	createStackNavigator,
+	StackNavigationEventMap,
+	StackNavigationOptions,
+} from "@react-navigation/stack"
+import { withLayoutContext } from "expo-router"
+import { View } from "react-native"
 
-export default () => <Slot screenOptions={{ headerShown: false }} />
+const { Navigator } = createStackNavigator()
+
+export const JsStack = withLayoutContext<
+	StackNavigationOptions,
+	typeof Navigator,
+	StackNavigationState<ParamListBase>,
+	StackNavigationEventMap
+>(Navigator)
+export default () => <JsStack screenOptions={{ headerShown: false }} />
--- src/app/bottom/index.tsx
+++ src/app/bottom/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native"
 
 export default function BottomIndex() {
 	return (
-		<View className="items-center gap-2 pb-safe">
+		<View className="flex-1 items-center gap-2 bg-white pb-safe">
 			<Text className="text-2xl">Bottom initial screen</Text>
 			<Link href="/bottom/other">
 				<Text>Other bottom screen</Text>
